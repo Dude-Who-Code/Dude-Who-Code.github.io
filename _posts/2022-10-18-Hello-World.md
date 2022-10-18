80c0a502b878e31c0afc09f56f372bc83fcfd623
sriram23 <sriramsubramanian23@gmail.com>
2022-10-18 21:30:41 +0530

    Added a sample blog

---
+++ _posts/2022-10-18-Hello-World.md
@@ -0,0 +1,4 @@
+# Hello World!
+Welcome to my first blog!
+
+It is very great hosting a Jekyll blog and writing in it!!
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Hello World",
+        "excerpt":"Hello World!  Welcome to my first blog!   It is very great hosting a Jekyll blog and writing in it!!  ","categories": [],
+        "tags": [],
+        "url": "/Hello-World/",
+        "teaser": null
+      }]
